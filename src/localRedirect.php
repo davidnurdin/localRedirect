5a7746c15c66b6c868fdb20de498e1f7446230dc
davidnurdin <david.nurdin@respawnsive.com>
2023-07-12 16:02:17 +0200

    ok

--- src/localRedirect.php
+++ src/localRedirect.php
@@ -1,10 +1,11 @@
 #!/usr/local/bin/php
 <?php
 
+use Psr\Http\Message\ResponseInterface;
 use React\Dns\Query\CoopExecutor;
 use React\Dns\Query\RetryExecutor;
 use React\Dns\Query\TimeoutExecutor;
-use React\Dns\Query\UdpTransportExecutor;
+use React\Dns\Query\UdpTransportExecutor;use React\Stream\CompositeStream;use React\Stream\ThroughStream;
 
 error_reporting(E_ERROR);
 require __DIR__ . '/vendor/autoload.php';
@@ -19,52 +20,44 @@ $executor = new CoopExecutor(
     )
 );
 $dns = new React\Dns\Resolver\Resolver($executor);
+$connector = new React\Socket\Connector(array(
+    'dns' => $dns,
+    'tcp' => array(),
+    'tls' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false
+    )
+));
 
-$http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) use ($dns)  {
+$http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) use ($connector) {
+    return new React\Promise\Promise(function ($resolve, $reject) use ($connector,$request) {
 
-    // On crée un connector qui va utilisé notre resolver dns ET qui va ignoré les certifs HTTPS
-    $connector = new React\Socket\Connector(array(
-        'dns' => $dns,
-        'tcp' => array(
-        ),
-        'tls' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false
-        )
-    ));
+/*        var_dump($request->getMethod());
+        var_dump($request->getUri());
+        var_dump($request->getHeaders());
+        var_dump($request->getBody());*/
 
-    // On crée notre client
-    $client = new React\Http\Browser($connector);
-    $promise = $client->get($request->getUri())->then(function (Psr\Http\Message\ResponseInterface $response) use ($request) {
-        return $response->getBody() ;
-    }, function (Exception $e) use ($request) {
-        echo 'Error: ' . $e->getMessage() . " => " . $request->getUri() .  PHP_EOL;
-    }) ;
+        var_dump($request->getBody());
 
-    try {
-        $response = React\Async\await($promise);
-        $response = React\Http\Message\Response::plaintext(
-            $response
-        );
+        (new React\Http\Browser($connector))->request($request->getMethod(),$request->getUri(),$request->getHeaders(),$request->getBody())->then(function (ResponseInterface $response) use ($resolve) {
+            $resolve(new React\Http\Message\Response(
+                React\Http\Message\Response::STATUS_OK,
+                $response->getHeaders(),
+                $response->getBody()
+            )) ;
 
-    } catch (Exception $e) {
-        echo "E2:" . $e->getMessage() . PHP_EOL ;
-
-        $response = new React\Http\Message\Response(
-            React\Http\Message\Response::STATUS_NOT_FOUND,
-            array(
-                'Content-Type' => 'text/html'
-            ),
-            "<html>Error 404</html>\n"
-        );
-
-    }
-
-
-    return $response ;
-
-
+        }, function (Exception $e) use ($resolve) {
+            // bad
+            $resolve(new React\Http\Message\Response(
+                React\Http\Message\Response::STATUS_BAD_GATEWAY,
+                array(
+                    'Content-Type' => 'text/plain'
+                ),
+                $e->getMessage()
+            ));
+        });
+    });
 });
-$socket = new React\Socket\TcpServer('127.0.0.1:80');
+$socket = new React\Socket\TcpServer('0.0.0.0:80');
 $http->listen($socket);
 echo "Server running" . PHP_EOL;
